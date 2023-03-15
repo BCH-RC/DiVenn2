23e6b140b597d4986d26bd8ec43722ace832ec34
Dane-Z <80366234+Dane-Z@users.noreply.github.com>
2023-03-15 12:11:42 -0700

    Delete SELECT DISTINCT(genes.ensembl_id) AS id,.sql

--- Code/SELECT DISTINCT(genes.ensembl_id) AS id,.sql
+++
@@ -1,6 +0,0 @@
-SELECT DISTINCT(genes_id_mapping.ensembl_id) AS id, genes.gene_desc, group_concat(distinct pathways.path_desc SEPARATOR '\\n') as pathway, group_concat(distinct pathways.path_url SEPARATOR '\\n') as urls, genes_id_mapping.ncbi_id
-              FROM genes
-              INNER JOIN pathways_gene_mapping ON pathways_gene_mapping.gene_id = genes.ensembl_id
-              INNER JOIN pathways ON pathways_gene_mapping.pathway_id = pathways.pathway_id 
-              INNER JOIN species ON pathways.species_id = species.species_id
-              WHERE genes_id_mapping.ncbi_id in ("839523", "839106", "838517") Group By id, genes.gene_desc, genes_id_mapping.ncbi;
