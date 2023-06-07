c9bd131e68e899f95bfcddf6c41f889c9fd6ca8f
Dane-Z <80366234+Dane-Z@users.noreply.github.com>
2023-06-07 16:53:48 -0700

    Delete Code/sql directory

--- Code/sql/ALTER_TABLE_pathway_creation_the_symbols.sql
+++
@@ -1,3 +0,0 @@
-USE divenn_db;
-alter table pathway add column symbol varchar(255);
-update pathway p set p.symbol=(select substring(p.gene_desc, 1, locate(';', p.gene_desc)-1));
--- Code/sql/CREATE_TABLE_pathway_details.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE pathway_details AS
-SELECT DISTINCT(p.kegg_id), p.gene_desc, p.pathway, p.path_url, in_path, not_in_path
-FROM pathway p LEFT JOIN ncbi2kegg n ON p.kegg_id = n.kegg_id LEFT JOIN uniprot2kegg u ON p.kegg_id=u.kegg_id
