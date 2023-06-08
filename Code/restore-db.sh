a07e0a25a08b2b488aed12c596b2d7392b581f16
Dane-Z <80366234+Dane-Z@users.noreply.github.com>
2023-06-07 17:00:29 -0700

    Delete restore-db.sh

--- Code/restore-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "CREATE DATABASE divenn;" | docker exec -i -e MYSQL_PWD=divenn bch-divenn-mysql-1 mysql -u root
-tar -JxOf ~/Downloads/divenn_db_20220304.tar.xz | \
-    cat <(echo "SET AUTOCOMMIT=0, UNIQUE_CHECKS=0, FOREIGN_KEY_CHECKS=0;") \
-        - \
-        <(echo "SET AUTOCOMMIT=1, UNIQUE_CHECKS=1, FOREIGN_KEY_CHECKS=1; COMMIT;") | \
-    docker exec -i -e MYSQL_PWD=divenn bch-divenn-mysql-1 mysql -u root divenn
