c84bd25b5e01aff60d4040d19558d883f3c3ffc4
Maryam Labaf <40154433+MaryamLabaf@users.noreply.github.com>
2025-03-06 12:02:03 -0500

    Update run_preprocess.sh

--- scRNAseq_preprocessing/run_preprocess.sh
+++ scRNAseq_preprocessing/run_preprocess.sh
@@ -4,7 +4,7 @@ export PYTHON_X=3.6.5
 
 file_path="/lab-share/RC-DST-Bioinfo-e2/Public/Chunhui/I26_DiVenn2/Data/p238.h5ad"
 output_dir="/lab-share/RC-DST-Bioinfo-e2/Public/Maryam/I26_DiVenn2"
-python preprocess_h5ad.py -i ${file_path} \
+python3 preprocess_h5ad.py -i ${file_path} \
                           -c "group" \
                           -g "celltype" \
                           -o "p238_divenne.csv" \
