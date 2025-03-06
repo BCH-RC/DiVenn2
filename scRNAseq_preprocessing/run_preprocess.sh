99b4d3dbb2e12ec2da818dc1acdaffb9909b2d15
Maryam Labaf <40154433+MaryamLabaf@users.noreply.github.com>
2025-03-06 12:01:29 -0500

    Create run_preprocess.sh

---
+++ scRNAseq_preprocessing/run_preprocess.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source /programs/biogrids.shrc
+export PYTHON_X=3.6.5
+
+file_path="/lab-share/RC-DST-Bioinfo-e2/Public/Chunhui/I26_DiVenn2/Data/p238.h5ad"
+output_dir="/lab-share/RC-DST-Bioinfo-e2/Public/Maryam/I26_DiVenn2"
+python preprocess_h5ad.py -i ${file_path} \
+                          -c "group" \
+                          -g "celltype" \
+                          -o "p238_divenne.csv" \
+                          -fc 0.2 \
+                          -pct 0.01 \
+                          -p 0.05 \
+                          -w ${output_dir}
