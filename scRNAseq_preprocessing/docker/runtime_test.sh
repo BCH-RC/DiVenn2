5f46ea445204b057b0fef5975df70cb9e087922d
Maryam Labaf <40154433+MaryamLabaf@users.noreply.github.com>
2026-03-18 08:53:58 -0400

    Update runtime_test.sh

--- scRNAseq_preprocessing/docker/runtime_test.sh
+++ scRNAseq_preprocessing/docker/runtime_test.sh
@@ -10,7 +10,7 @@ output_dir="/lab-share/RC-DST-Bioinfo-e2/Public/Maryam/I26_DiVenn2/DiVenn2-main/
 
 # ---- R version ----
 file_path="/lab-share/RC-DST-Bioinfo-e2/Public/Maryam/I26_DiVenn2/DiVenn2-main/scRNAseq_preprocessing/case_study/p111_casestudy_ctx.rds"
-# NOTE: Seurat v4:findMarkers log10-FC cutoff = 0.25 ~ Seurat v5:FindMarkers log2-FC cutoff = 0.83 (log_2(x) =log10(x)/log10(2))
+# NOTE: Seurat v4:findMarkers ln-FC cutoff = 0.25 ~ Seurat v5:FindMarkers log2-FC cutoff = 0.36 (log2(x) =log(x)/log(2))
 Rscript Preprocessing_Seuratobj_dev.R \
   -w "$output_dir" \
   -i "$file_path" \
@@ -18,8 +18,8 @@ Rscript Preprocessing_Seuratobj_dev.R \
   -g "celltypes" \
   -x "FXPM:CON,FXS:CON" \
   -m "MAST" \
-  -f 0.83 \
-  -r 0.1 \
+  -f 0.36 \
+  -r 0.01 \
   -v 0.05 \
   -l "$Gene_list" \
   -d keep \
